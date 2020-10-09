af80d65d9891dce6c434d17a3d7253a616790a66
s1719890 <j.f.cheung@umail.leidenuniv.nl>
2020-10-09 22:05:11 +0200

    fix

--- PM/tentamen.cpp
+++ PM/tentamen.cpp
@@ -5,20 +5,16 @@ using namespace std;
 
 int x, y;
 
-int ot (int r){ r+= 2; return r; }
-
-int sien (int u, int v){
-	int i; y++; for (i = 0 ; i<= 1000; i++) u +- ot(v);
-	
-	v = v+6; cout << i << "," << u << "," << v << endl;
-	return u + v + 7;
-}
+int ot (int r) { r += 2; return r; }//ot
+int sien (int u, int v) {
+	int i; y++; for ( i = 0; i <= 1000; i++ ) u += ot (v);
+	v = v + 6; cout << i << "," << u << "," << v << endl; return u + v + 7;
+}//sien
 
 
 
 
 int main (){
-	x = 4; y = 0;
-	cout << sien (x,y) <<"," cout << x-1 << "," << y << endl;
-
+x = 4; y = 0; cout << sien (x,y) << ","; cout << x-1 << "," << y << endl;
+	return 0;
 }
